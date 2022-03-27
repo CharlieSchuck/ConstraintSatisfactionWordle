9b82a8b384ab8e7ed779ef9e135ac70cd2a8daa1
Eric <luigilinked@gmail.com>
2022-03-27 12:36:51 -0400

    Initial commit.

---
+++ Dictionary Downloader/src/DictionaryDownloader.java
@@ -0,0 +1,10 @@
+/*
+    Eric Grandizio
+    Dictionary Downloader
+*/
+
+public class DictionaryDownloader {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
