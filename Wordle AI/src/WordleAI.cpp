3ac826821485c73f7e68539670e87d51f7ba91ab
Eric <luigilinked@gmail.com>
2022-04-14 14:07:12 -0400

    Minor reformatting, documentation updates, and bugfixes

--- Wordle AI/src/WordleAI.cpp
+++ Wordle AI/src/WordleAI.cpp
@@ -1,65 +1,83 @@
 #include "WordleAI.h"
-#include "WordleSim.h"
 
 #include <algorithm>
 #include <iostream>
 #include <queue>
+
+#include "WordleSim.h"
+
 // ================================================================================================================================ //
+
 WordleAI::WordleAI(const Dictionary& dict_g) 
-	: dict{ dict_g } { //weird and gross. Man, C++ is the worst. 
+	:
+	dict{ dict_g }
+{}
 
-}
+// ================================================================================================================================ //
 
-std::string WordleAI::makeGuess(std::size_t try_count) {
+std::string WordleAI::makeGuess(const std::size_t try_count)
+{
 	//Local BEAM! -- find the four guesses that give the most information in the dictionary at hand.
 	//generate dictionaries that are the consequences of each of those guesses... find the four strongest in each of those dictionaries
 	//so on and so forth equal to the number of guesses remaining... 
 
-	for (std::size_t i = 0; i < dict.size(); i++) {
+	for (std::size_t i = 0; i < dict.size(); i++)
+	{
 
 	}
 
 	return ""; //"just to appease the compiler".
 }
 
-void WordleAI::updateDictionary(Results feedback, std::string& guess) {
-	for (std::size_t i = 0; i < feedback.size(); i++) {
-		Feedback f = feedback.at(i);
-		char p = f.letter;
-		Result q = f.result;
+// ================================================================================================================================ //
+
+void WordleAI::updateDictionary(const Results& feedback, const std::string& guess)
+{
+	for (std::size_t i = 0; i < feedback.size(); i++)
+	{
+		const Feedback f{ feedback.at(i) };
+		const char p{ f.letter };
+		const Result q{ f.result };
 		
-		switch (q) {
-			case Result::Correct: {
+		switch (q)
+		{
+			case Result::Correct:
+			{
 				erase_if([=](std::string& word) { return word.at(i) != p; });
-				break;
 			}
+			break;
 
-			case Result::Exists: {
+			case Result::Exists:
+			{
 				erase_if([=](std::string& word) { return word.at(i) == p; });
-				break;
 			}
+			break;
 
-			case Result::Invalid: {
+			case Result::Invalid:
+			{
 				erase_if([=](std::string& word) { return word.at(i) == p; });
-				auto n = std::count_if(feedback.begin(), feedback.end(), [=](Feedback fb) { return (fb.letter == p) && (fb.result != q); });
-				erase_if([=](std::string& word) { return std::count_if(word.begin(), word.end(), p) != n; });
-				break;
+
+				const auto n{ std::count_if(feedback.begin(), feedback.end(), [=](const Feedback fb) { return (fb.letter == p) && (fb.result != q); }) };
+				erase_if([=](std::string& word) { return std::count(word.begin(), word.end(), p) != n; });
 			}
-			
+			break;
 		}
 
 	}
 }
 
-void ai_play() {
+// ================================================================================================================================ //
 
-	// Dictionary of Hidden Words to be chosen.
+void ai_play()
+{
+	// Dictionary of Hidden Words that can be chosen.
 	const Dictionary dict_a{ load_dictionary("../Dictionaries/wordle-answers.txt") };
 
-	// Dictionary of Valid Word to be guessed.
+	// Dictionary of Valid Word that can be guessed.
 	const Dictionary dict_g{ load_dictionary("../Dictionaries/wordle-guesses.txt") };
 
 	WordleAI ai{ dict_g };
+
 	WordleSim sim{ dict_a };
 	Results feedback{ sim.word_length() };
 
@@ -69,15 +87,16 @@ void ai_play() {
 	// Play continues infinitely until the Game is Won.
 	while (!feedback.is_won())
 	{
-		std::cout << "\n\n-- Guess " << (sim.tries() + 1) << " --\n";
+		std::cout << "\n\n-- Turn " << (sim.tries() + 1) << " --\n";
+		std::cout << "AI Dictionary Size: " << ai.dict.size() << "\n";
 
-		std::string guess = ai.makeGuess(sim.tries());
-		feedback = sim.make_guess(guess);
-		ai.updateDictionary(feedback, guess);
-
-
+		const std::string guess{ ai.makeGuess(sim.tries()) };
 		std::cout << "  Guess: " << guess << '\n';
+
+		feedback = sim.make_guess(guess);
 		std::cout << "Results: " << feedback.str() << '\n';
+		
+		ai.updateDictionary(feedback, guess);
 	}
 	std::cout << "\n\n==== YOU WIN! ====\n\n";
 
--- Wordle AI/src/WordleAI.h
+++ Wordle AI/src/WordleAI.h
@@ -1,23 +1,44 @@
 #pragma once
+
+#include <cstddef>
+
 #include <string>
+#include <algorithm>
+
 #include "WordleSim.h"
+
 // ================================================================================================================================ //
 
 class WordleAI
 {
-public:
-	Dictionary dict;
-	WordleAI(const Dictionary& dict_g);
-	std::string makeGuess(std::size_t try_count);
-	void updateDictionary(Results feedback, std::string& guess);
+public: // Variables
 
-	template<typename T>
-	void erase_if(T condition) {
+	// The Dictionary holding valid guesses.
+	Dictionary dict;
+
+public: // Functions
+
+	// Constructs the AI with the given Dictionary.
+	WordleAI(const Dictionary& dict_g);
+
+	// Returns a Guess from the Dictionary.
+	std::string makeGuess(const std::size_t try_count);
+
+	// Analyzes the Feedback on the given guess and updates the Dictionary accordingly.
+	void updateDictionary(const Results& feedback, const std::string& guess);
+
+	// Erases the words that meet the given condition from the dictionary.
+	template<class Predicate>
+	void erase_if(Predicate&& condition)
+	{
 		dict.erase(std::remove_if(dict.begin(), dict.end(), condition), dict.end());
 	}
+
 };
 
+// ================================================================================================================================ //
 
+// Runs the AI on a random word.
 void ai_play();
 
 // ================================================================================================================================ //
